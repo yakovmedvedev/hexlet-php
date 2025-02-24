f18720b6079274c97f4a258fa1ea2afa7ebcfe37
Yakov Medvedev <yakovmedvedev@gmail.com>
2025-02-25 02:13:15 +0300

    composer-files + index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
 
-echo "Hello, Hexlet!";
+// Файл не включается напрямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
