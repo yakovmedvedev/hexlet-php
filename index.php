f22b5a1fb7af63ab2ac29eb861ea5b7b411104e5
Yakov Medvedev <yakovmedvedev@gmail.com>
2025-02-25 03:35:11 +0300

    tried to change the path

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 // Файл не включается напрямую
 // Он загрузится автоматически благодаря автозагрузке
 use Hexlet\Php\Runner;
+//use Runner\run;
 
 print_r(Runner\run());
